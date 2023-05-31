08be4226765897b6dec6f30526c8a3c0fe060419
Karan Anand <thekarananand@gmail.com>
2023-05-31 09:06:08 +0530

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,3 +1,11 @@
+# Installing/Updating Winget
+
+$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets" | Where-Object "browser_download_url" -Match '.msixbundle' | Select-Object -ExpandProperty "browser_download_url"
+Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
+Add-AppxPackage -Path "Setup.msix"
+Remove-Item "Setup.msix"
+
 # Removing All Store Apps
 
 Get-AppxPackage | Remove-AppxPackage
