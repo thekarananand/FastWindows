e3802ceaae82d74229774e2a6c5e0866a73a4043
thekarananand <thekarananand@gmail.com>
2023-06-27 15:45:57 +0530

    clear junk

--- start.ps1
+++ start.ps1
@@ -242,10 +242,32 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name "AutoUpdateEnabled" -Type DWord
 
 
 # Removing Unnecessary Shortcuts
-
 Remove-Item $env:HOMEPATH\AppData\Roaming\Microsoft\Windows\Start` Menu\Programs\OneDrive.lnk
 Remove-Item $env:HOMEPATH\AppData\Roaming\Microsoft\Windows\Start` Menu\Programs\Firefox` Private` Browsing.lnk
 Remove-Item C:\ProgramData\Microsoft\Windows\Start` Menu\Programs\Firefox` Private` Browsing.lnk
+
+
+# Clear Internet Explorer cache
+Clear-IECache -Confirm:$false
+
+# Clear temporary files
+Remove-Item -Path "$env:TEMP\*" -Force -Recurse
+Remove-Item -Path "$env:LOCALAPPDATA\Temp\*" -Force -Recurse
+
+# Clear user profile temporary files
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Temp\*" -Force -Recurse
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Force -Recurse
+
+# Clear Windows thumbnail cache
+Stop-Process -Name explorer -Force
+$thumbnailCachePath = "$env:LOCALAPPDATA\Microsoft\Windows\Explorer"
+Remove-Item -Path "$thumbnailCachePath\thumbcache_*.db" -Force
+Remove-Item -Path "$thumbnailCachePath\iconcache_*.db" -Force
+Start-Process explorer
+
+# Clear Recycle Bin
+Clear-RecycleBin -Confirm:$false
+
 
 Set-ExecutionPolicy -ExecutionPolicy Restricted -Force
 
