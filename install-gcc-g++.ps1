dc8869f425e97cdfa69d77e5c728b028a4541e49
Karan Anand <thekarananand@gmail.com>
2023-07-01 09:42:53 +0530

    testing

--- install-gcc-g++.ps1
+++ install-gcc-g++.ps1
@@ -1,20 +1,6 @@
-# Download the GCC and G++ installers
-$gcc_installer = "https://sourceforge.net/projects/mingw/files/installer/mingw-w64-x86_64-gcc-11.2.0-win32-setup.exe"
-$g++_installer = "https://sourceforge.net/projects/mingw/files/installer/mingw-w64-x86_64-g++-11.2.0-win32-setup.exe"
-
-# Install the GCC and G++ compilers
-Start-Process $gcc_installer
-Start-Process $g++_installer
-
-# Get the current PATH variable
-$current_path = $env:PATH
-
-# Add the path to the GCC and G++ installers to the PATH variable
-$current_path += ";$env:TEMP\mingw64\bin"
-
-# Set the PATH variable
-Set-EnvironmentVariable -Name "PATH" -Value $current_path
-
-# Verify that the GCC and G++ compilers are installed
-gcc --version
-g++ --version
+$URL = "https://sourceforge.net/projects/mingw/files/Installer/mingw-get-setup.exe/download"
+$Output = "mingw-get-setup.exe"
+Invoke-WebRequest -Uri $URL -OutFile $Output
+Start-Process -FilePath $Output -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+$Path = "C:\MinGW\bin"
+$Env:Path += ";$Path"
