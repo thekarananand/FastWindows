6ca9a25695dff0d9adf72290db849fc3115eb84a
Karan Anand <thekarananand@gmail.com>
2023-07-04 12:12:24 +0530

    Delete install-gcc-g++.ps1

--- install-gcc-g++.ps1
+++
@@ -1,6 +0,0 @@
-$URL = "https://sourceforge.net/projects/mingw/files/Installer/mingw-get-setup.exe/download"
-$Output = "mingw-get-setup.exe"
-Invoke-WebRequest -Uri $URL -OutFile $Output
-Start-Process -FilePath $Output -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
-$Path = "C:\MinGW\bin"
-$Env:Path += ";$Path"
