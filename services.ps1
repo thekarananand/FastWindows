ddada9592496436a5cb7464773c3111a56b86f3e
Karan Anand <Karan Anand>
2023-09-16 10:10:09 +0530

    added more services

--- services.ps1
+++ services.ps1
@@ -1,12 +1,38 @@
-Set-Service -Name 'AppVClient' -StartupType Disabled                        # Disabled App-V Client for virtualized applications.
-Set-Service -Name 'AssignedAccessManagerSvc' -StartupType Disabled          # Disables the Assigned Access Manager Service, which restrict users to running specific applications.
-Set-Service -Name 'DiagTrack' -StartupType Disabled                         # Disables the Diagnostic and Usage Tracking service
-Set-Service -Name 'DialogBlockingService' -StartupType Disabled             # Disables the Dialog Blocking Service, which prevents dialog boxes from appearing when you are playing games or watching videos in full screen mode. Solve your Errors not hide them
-Set-Service -Name 'NetTcpPortSharing' -StartupType Disabled                 # Disables the Net.TCP Port Sharing service, which allows multiple applications to share the same TCP/IP port. Security Vulnerability.
-Set-Service -Name 'RemoteAccess' -StartupType Disabled                      # Disables the Remote Access service. Security Vulnerability.
-Set-Service -Name 'RemoteRegistry' -StartupType Disabled                    # Disables the Remote Registry service. Security Vulnerability.
-Set-Service -Name 'UevAgentService' -StartupType Disabled                   # Disables the User Experience Virtualization (UEV) Agent Service, which allows you to synchronize your user settings between different computers.
-Set-Service -Name 'shpamsvc' -StartupType Disabled                          # Disables preloading frequently used applications and files into memory
-# Set-Service -Name 'ssh-agent' -StartupType Disabled
-Set-Service -Name 'tzautoupdate' -StartupType Disabled                      # Disables the Telemetry and Compatibility Assistant (TACO) Auto-Update service
-Set-Service -Name 'uhssvc' -StartupType Disabled                            # Disables the User Health Service
+# Disabled Services
+
+Set-Service -Name 'wlidsvc' -StartupType Disabled                           # Windows Live ID service
+Set-Service -Name 'wlpasvc' -StartupType Disabled                           # Windows Live Authentication service
+Set-Service -Name 'wmiApSrv' -StartupType Disabled                          # WMI Application Server service, Provides a remote interface for managing and monitoring Windows computers
+Set-Service -Name 'workfolderssvc' -StartupType Disabled                    # Work Folders service
+Set-Service -Name 'AppVClient' -StartupType Disabled                        # App-V Client for virtualized apps.
+Set-Service -Name 'AssignedAccessManagerSvc' -StartupType Disabled          # Assigned Access Manager Service, Manage Restriction for specified users to running specific apps.
+Set-Service -Name 'DiagTrack' -StartupType Disabled                         # Diagnostic and Usage Tracking service
+Set-Service -Name 'DialogBlockingService' -StartupType Disabled             # Dialog Blocking Service, Prevents dialog boxes from appearing while playing games or watching videos in full screen mode. Solve your Errors not hide them
+Set-Service -Name 'NetTcpPortSharing' -StartupType Disabled                 # Net.TCP Port Sharing service, which allows multiple applications to share the same TCP/IP port. Security Vulnerability.
+Set-Service -Name 'RemoteAccess' -StartupType Disabled                      # Remote Access service. Security Vulnerability.
+Set-Service -Name 'RemoteRegistry' -StartupType Disabled                    # Remote Registry service. Security Vulnerability.
+Set-Service -Name 'UevAgentService' -StartupType Disabled                   # User Experience Virtualization (UEV) Agent Service, which allows you to synchronize your user settings between different computers.
+Set-Service -Name 'shpamsvc' -StartupType Disabled                          # Preloading frequently used applications and files into memory
+Set-Service -Name 'tzautoupdate' -StartupType Disabled                      # Telemetry and Compatibility Assistant (TACO) Auto-Update service
+Set-Service -Name 'uhssvc' -StartupType Disabled                            # User Health Service
+
+
+
+# Xbox Services
+Set-Service -Name 'XblAuthManager' -StartupType Disabled
+Set-Service -Name 'XblGameSave' -StartupType Disabled
+Set-Service -Name 'XboxGipSvc' -StartupType Disabled
+Set-Service -Name 'XboxNetApiSvc' -StartupType Disabled
+
+
+
+# Services Disabled on boot, will start on invoking
+Set-Service -Name 'ssh-agent' -StartupType Manual                           # ssh-agent
+Set-Service -Name 'wuauserv' -StartupType Manual                            # Windows Update service
+
+
+
+# Seem Essential
+# Set-Service -Name 'wscsvc' -StartupType Manual                            # Windows Search service
+# Set-Service -Name 'wudfsvc' -StartupType Manual                           # Windows Defender service
+# Set-Service -Name 'wisvc' -StartupType Manual                             # Windows Installer service
