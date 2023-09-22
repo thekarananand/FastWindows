2b0d456fec4d3664d4126647fed0a388d76d3826
Karan Anand <thekarananand@gmail.com>
2023-09-23 01:44:40 +0530

    testing stage 2

--- Stage2_RemoveWin32Apps.ps1
+++ Stage2_RemoveWin32Apps.ps1
@@ -2,21 +2,24 @@ Write-Host "Stage 2 : REMOVING EDGE, ONEDRIVE, AND OTHER WIN32 APPS"
 Write-Host "=========================================================`n"
 
 # Edge 
-irm https://raw.githubusercontent.com/thekarananand/FastWindows/main/Scripts/edgeremoval.bat | iex
+
+Start-Process -FilePath "cmd.exe" -ArgumentList "/C irm https://raw.githubusercontent.com/thekarananand/FastWindows/main/Scripts/edgeremoval.bat" -Wait
+
+# irm https://raw.githubusercontent.com/thekarananand/FastWindows/main/Scripts/edgeremoval.bat | iex
+
 cd "C:\Program Files (x86)\Microsoft\EdgeWebView\Application\1*\Installer"
 .\setup.exe --uninstall --msedgewebview --system-level --verbose-logging --force-uninstall
 Remove-Item -Path "C:\Program Files (x86)\Microsoft\Edge" -Recurse -Force
 Remove-Item -Path "C:\Program Files (x86)\Microsoft\EdgeUpdate" -Recurse -Force
 
 # Teams
-winget uninstall Microsoft.Teams
+winget uninstall Microsoft.Teams --accept-source-agreements --accept-package-agreements
 
 # OneDrive
-winget uninstall Microsoft.OneDriveSync_8wekyb3d8bbwe
-winget uninstall Microsoft.OneDrive
+winget uninstall Microsoft.OneDriveSync_8wekyb3d8bbwe --accept-source-agreements --accept-package-agreements
+winget uninstall Microsoft.OneDrive --accept-source-agreements --accept-package-agreements
 
-takeown /f C:\scratchdir\Windows\System32\OneDriveSetup.exe
-icacls C:\scratchdir\Windows\System32\OneDriveSetup.exe /grant Administrators:F /T /C
+takeown /f C:Windows\System32\OneDriveSetup.exe
 del /f /q /s "C:\Windows\System32\OneDriveSetup.exe"
 
 taskkill /f /im OneDrive.exe
