41c4598a41b894d962534d5e81be040682667375
Karan Anand <thekarananand@gmail.com>
2023-09-23 21:09:51 +0530

    Fix Start Content

--- Stage5_RegeditTweaks.ps1
+++ Stage5_RegeditTweaks.ps1
@@ -19,7 +19,14 @@ foreach ($Tweak in $ContentDeliveryManagerTweaks_SetZero) {
     New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name $Tweak -Value 0 -Type DWord -Force
 }
 
+if (!(Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Force
+}
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\CloudContent" -Name 'DisableWindowsConsumerFeatures' -Value 1 -Type DWord -Force
+
+if (!(Test-Path -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start" -Force
+}
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\Start" -Name 'ConfigureStartPins' -Value '{"pinnedList": [{}]}' -Type String -Force
 
 Write-Host "Disabled  : Sponsored Apps in Start"
