96d81b5dd0709590afacad206624d61320bc5b21
Karan Anand <thekarananand@gmail.com>
2023-09-23 09:43:52 +0530

    tweaked stage 4

--- Stage4_RemoveOptionalFeatures.ps1
+++ Stage4_RemoveOptionalFeatures.ps1
@@ -2,50 +2,50 @@ Write-Host "`nStage 4 : REMOVED OPTIONAL FEATURES"
 Write-Host "========================================================="
 
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~en-US~11.0.22621.1  | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~~11.0.22621.1265  | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~en-US~11.0.22621.1 
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-InternetExplorer-Optional-Package~31bf3856ad364e35~amd64~~11.0.22621.1265 
     
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:Browser.InternetExplorer~~~~0.0.11.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:Browser.InternetExplorer~~~~0.0.11.0
 
     Write-Host "Removed   : Internet Explorer"
 
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.WordPad~~~~0.0.1.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.WordPad~~~~0.0.1.0
     Write-Host "Removed   : WordPad"
 
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.PowerShell.ISE~~~~0.0.1.0
     Write-Host "Removed   : PowerShell ISE"
 
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.Notepad.System~~~~0.0.1.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:Microsoft.Windows.Notepad.System~~~~0.0.1.0
     Write-Host "Removed   : NotePad (System)"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~wow64~en-US~10.0.22621.1 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~wow64~~10.0.22621.1 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~wow64~en-US~10.0.22621.1
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-MediaPlayer-Package~31bf3856ad364e35~wow64~~10.0.22621.1
 
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:Media.WindowsMediaPlayer~~~~0.0.12.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:Media.WindowsMediaPlayer~~~~0.0.12.0
 
     Write-Host "Removed   : Windows Media Player"
 
-    dism /Online /Remove-Capability /NoRestart /CapabilityName:App.StepsRecorder~~~~0.0.1.0 | Out-Null
+    dism /Online /Remove-Capability /NoRestart /CapabilityName:App.StepsRecorder~~~~0.0.1.0
     Write-Host "Removed   : StepsRecorder"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Kernel-LA57-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Kernel-LA57-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
     Write-Host "Removed   : Windows Kernel LA57"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Kernel-LA57-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Kernel-LA57-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
     Write-Host "Removed   : Windows Kernel LA57"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Wallpaper-Content-Extended-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-Wallpaper-Content-Extended-FoD-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
     Write-Host "Removed   : Extended Wallpaper Content"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-TabletPCMath-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-TabletPCMath-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
     Write-Host "Removed   : Tablet PC Math"
 
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-Handwriting-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-OCR-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-Speech-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
-    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-TextToSpeech-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265 | Out-Null
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-Handwriting-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-OCR-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-Speech-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
+    dism /Online /Remove-Package /NoRestart /PackageName:Microsoft-Windows-LanguageFeatures-TextToSpeech-en-us-Package~31bf3856ad364e35~amd64~~10.0.22621.1265
     Write-Host "Removed   : Language Features"
 
 
