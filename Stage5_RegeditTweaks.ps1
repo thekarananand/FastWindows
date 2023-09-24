80d9ff48ecdf1666168190100d4465d0be641a42
Karan Anand <thekarananand@gmail.com>
2023-09-24 13:27:01 +0530

    added Privacy Realting fixes

--- Stage5_RegeditTweaks.ps1
+++ Stage5_RegeditTweaks.ps1
@@ -73,10 +73,88 @@ foreach ($Tweak in $Windows_Search_SetZero) {
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Value 1 -PropertyType DWORD -Force | Out-Null
 
 if (!(Test-Path -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings")) {
-    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" -Force
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" -Force  | Out-Null
 }
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" -Name "IsDynamicSearchBoxEnabled" -Value 0 | Out-Null
 
 Write-Host "Disabled  : Windows Search History"
 Write-Host "Disabled  : Bing/Web Search in Windows Search"
 Write-Host "Disabled  : Dynamic Search Highlights"
+
+#---------------------------------------------------------------------------------------
+# Disable Advertising ID
+
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo")) {
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Force | Out-Null
+} 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" -Name Enabled -Value 0 | Out-Null\
+
+Write-Host "Disabled  : Advertising ID"
+
+#---------------------------------------------------------------------------------------
+# Disable Teams bubble chat
+
+if (!(Test-Path -Path "HKCU:\Software\Policies\Microsoft\Office\Teams")) {
+    New-Item -Path "HKCU:\Software\Policies\Microsoft\Office\Teams" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Office\Teams" -Name "ChatButtonVisibility" -Value 0 | Out-Null
+
+if (!(Test-Path -Path "HKCU:\Software\Microsoft\Office\Teams\LivePreview")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Office\Teams\LivePreview" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Office\Teams\LivePreview" -Name "Enabled" -Value 0 | Out-Null
+
+if (!(Test-Path -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value 0 | Out-Null
+
+Write-Host "Disabled  : Teams Bubble Chat"
+
+#---------------------------------------------------------------------------------------
+# Enable Dark Theme for Apps
+If (!(Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize")) {
+    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 | Out-Null
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 | Out-Null
+
+Write-Host "Enabled   : Dark Mode"
+
+#---------------------------------------------------------------------------------------
+# Disable the News & Feed
+If (!(Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize")) {
+    New-Item -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" -Name "ShellFeedsTaskbarViewMode" -Value 2 | Out-Null
+
+Write-Host "Disabled  : News & Feed"
+
+#---------------------------------------------------------------------------------------
+# Disable Telemetry & Diagnostic data
+if (!(Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0 | Out-Null
+
+Write-Host "Disabled  : Telemetry & Diagnostic data"
+
+#---------------------------------------------------------------------------------------
+# Disable Find my device
+if (!(Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SettingSync")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SettingSync" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\SettingSync" -Name "DisableSettingSyncUserOverride" -Value 1 | Out-Null
+
+Write-Host "Disabled  : Find my device"
+
+#---------------------------------------------------------------------------------------
+# Disable Inking & typing personalization
+if (!(Test-Path -Path "HKCU:\Software\Microsoft\InputPersonalization")) {
+    New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Value 1 | Out-Null
+
+Write-Host "Disabled  : Inking & typing personalization"
+
+#---------------------------------------------------------------------------------------
