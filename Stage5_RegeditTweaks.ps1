8148590073492504d40f4bd3166810daf04a99ba
Karan Anand <thekarananand@gmail.com>
2023-09-24 09:54:47 +0530

    added search fixes

--- Stage5_RegeditTweaks.ps1
+++ Stage5_RegeditTweaks.ps1
@@ -2,6 +2,7 @@ Write-Host "`nStage 5 : REGEDIT TWEAKS"
 Write-Host "========================================================="
 
 #----------------------------------------------------------------------------------------------------
+# Disabling Sponsored Apps in Start
 
 $ContentDeliveryManagerTweaks_SetZero = @(
     "ContentDeliveryAllowed",
@@ -36,4 +37,46 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\device\St
 Write-Host "Disabled  : Sponsored Apps in Start"
 
 #----------------------------------------------------------------------------------------------------
+# Disabling Stupid Behaviour of Windows Search
 
+
+$CurrentVerison_Search_SetZero = @(
+    "SearchHistoryEnabled",
+    "CortanaConsent",
+    "BingSearchEnabled",
+    "AllowSearchToUseLocation"
+)
+
+if (!(Test-Path -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Force | Out-Null
+}
+
+foreach ($Tweak in $CurrentVerison_Search_SetZero) {
+    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name $Tweak -Value 0 -Type DWord -Force | Out-Null
+}
+
+
+$Windows_Search_SetZero = @(
+    "ConnectedSearchUseWebOverMeteredConnections"
+    "AllowCortana"
+    "ConnectedSearchUseWeb"
+)
+
+if (!(Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+}
+
+foreach ($Tweak in $Windows_Search_SetZero) {
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name $Tweak -Value 0 -Type DWord -Force | Out-Null
+}
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Value 1 -PropertyType DWORD -Force | Out-Null
+
+if (!(Test-Path -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings")) {
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" -Force
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" -Name "IsDynamicSearchBoxEnabled" -Value 0
+
+Write-Host "Disabled  : Windows Search History"
+Write-Host "Disabled  : Bing/Web Search in Windows Search"
+Write-Host "Disabled  : Dynamic Search Highlights"
