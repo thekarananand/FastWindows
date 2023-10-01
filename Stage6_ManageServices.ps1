89c76313e7a0c8c91917744eeda42ef94660c6df
Karan Anand <thekarananand@gmail.com>
2023-10-01 08:44:02 +0530

    Restructured the Script

--- Stage6_ManageServices.ps1
+++ Stage6_ManageServices.ps1
@@ -1,39 +1,43 @@
 Write-Host "`nStage 7 : MANAGING SERVICES"
 Write-Host "========================================================="
 
-# Disabled Services
 
-Set-Service -Name 'wlidsvc' -StartupType Disabled                           # Windows Live ID service
-Set-Service -Name 'wlpasvc' -StartupType Disabled                           # Windows Live Authentication service
-Set-Service -Name 'wmiApSrv' -StartupType Disabled                          # WMI Application Server service, Provides a remote interface for managing and monitoring Windows computers
-Set-Service -Name 'workfolderssvc' -StartupType Disabled                    # Work Folders service
-Set-Service -Name 'AppVClient' -StartupType Disabled                        # App-V Client for virtualized apps.
-Set-Service -Name 'AssignedAccessManagerSvc' -StartupType Disabled          # Assigned Access Manager Service, Manage Restriction for specified users to running specific apps.
-Set-Service -Name 'DiagTrack' -StartupType Disabled                         # Diagnostic and Usage Tracking service
-Set-Service -Name 'DialogBlockingService' -StartupType Disabled             # Dialog Blocking Service, Prevents dialog boxes from appearing while playing games or watching videos in full screen mode. Solve your Errors not hide them
-Set-Service -Name 'NetTcpPortSharing' -StartupType Disabled                 # Net.TCP Port Sharing service, which allows multiple applications to share the same TCP/IP port. Security Vulnerability.
-Set-Service -Name 'RemoteAccess' -StartupType Disabled                      # Remote Access service. Security Vulnerability.
-Set-Service -Name 'RemoteRegistry' -StartupType Disabled                    # Remote Registry service. Security Vulnerability.
-Set-Service -Name 'UevAgentService' -StartupType Disabled                   # User Experience Virtualization (UEV) Agent Service, which allows you to synchronize your user settings between different computers.
-Set-Service -Name 'shpamsvc' -StartupType Disabled                          # Preloading frequently used applications and files into memory
-Set-Service -Name 'tzautoupdate' -StartupType Disabled                      # Telemetry and Compatibility Assistant (TACO) Auto-Update service
-Set-Service -Name 'uhssvc' -StartupType Disabled                            # User Health Service
+$Services_to_Disable = @(
+    'wlidsvc',                           # Windows Live ID service
+    'wlpasvc',                           # Windows Live Authentication service
+    'wmiApSrv',                          # WMI Application Server service, Provides a remote interface for managing and monitoring Windows computers
+    'workfolderssvc',                    # Work Folders service
+    'AppVClient',                        # App-V Client for virtualized apps.
+    'AssignedAccessManagerSvc',          # Assigned Access Manager Service, Manage Restriction for specified users to running specific apps.
+    'DiagTrack',                         # Diagnostic and Usage Tracking service
+    'DialogBlockingService',             # Dialog Blocking Service, Prevents dialog boxes from appearing while playing games or watching videos in full screen mode. Solve your Errors not hide them
+    'NetTcpPortSharing',                 # Net.TCP Port Sharing service, which allows multiple applications to share the same TCP/IP port. Security Vulnerability.
+    'RemoteAccess',                      # Remote Access service. Security Vulnerability.
+    'RemoteRegistry',                    # Remote Registry service. Security Vulnerability.
+    'UevAgentService',                   # User Experience Virtualization (UEV) Agent Service, which allows you to synchronize your user settings between different computers.
+    'shpamsvc',                          # Preloading frequently used applications and files into memory
+    'tzautoupdate',                      # Telemetry and Compatibility Assistant (TACO) Auto-Update service
+    'uhssvc',                            # User Health Service
+    'XblAuthManager',                    # Xbox Services
+    'XblGameSave',                       # Xbox Services
+    'XboxGipSvc',                        # Xbox Services
+    'XboxNetApiSvc',                     # Xbox Services
+)
 
+foreach ($Service in $Services_to_Disable) {
+    Set-Service -Name $Service -StartupType Disabled
+    Write-Host "Disabled  : $Service"
+}
 
+$Services_to_Manual = @(
+    'ssh-agent',                         # ssh-agent
+    'wuauserv',                          # Windows Update service
+)
 
-# Xbox Services
-Set-Service -Name 'XblAuthManager' -StartupType Disabled
-Set-Service -Name 'XblGameSave' -StartupType Disabled
-Set-Service -Name 'XboxGipSvc' -StartupType Disabled
-Set-Service -Name 'XboxNetApiSvc' -StartupType Disabled
-
-
-
-# Services Disabled on boot, will start on invoking
-Set-Service -Name 'ssh-agent' -StartupType Manual                           # ssh-agent
-Set-Service -Name 'wuauserv' -StartupType Manual                            # Windows Update service
-
-
+foreach ($Service in $Services_to_Manual) {
+    Set-Service -Name $Service -StartupType Manual
+    Write-Host "Manual    : $Service"
+}
 
 # Seem Essential
 # Set-Service -Name 'wscsvc' -StartupType Manual                            # Windows Search service
